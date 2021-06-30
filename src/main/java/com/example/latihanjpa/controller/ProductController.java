9a0037b8b7275cbe763c6e18c6b8a7099700fd7f
sidhiekardhi <SArdhiansah@gmail.com>
2021-06-30 16:21:52 +0700

    update product

--- src/main/java/com/example/latihanjpa/controller/ProductController.java
+++ src/main/java/com/example/latihanjpa/controller/ProductController.java
@@ -1,10 +1,15 @@
 package com.example.latihanjpa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.latihanjpa.entity.Product;
 import com.example.latihanjpa.service.ProductService;
 
@@ -18,5 +23,24 @@ public class ProductController {
 	public Product addProduct(@RequestBody Product product) {
 		return productService.saveProduct(product);
 	}
-
+	
+	@GetMapping("/getProducts")
+	public List<Product> findAllProduct(){
+		return productService.getProducts();
+	}
+	
+	@GetMapping("/getProductById/{id}")
+	public Product FindById(@PathVariable int id) {
+		return productService.getById(id);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable int id) {
+		return productService.deleteProduct(id);
+	}
+	
+	@PutMapping("/update")
+	public Product updateProduct(@RequestBody Product product) {
+		return productService.updateProduct(product);
+	}
 }
--- src/main/java/com/example/latihanjpa/service/ProductService.java
+++ src/main/java/com/example/latihanjpa/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.example.latihanjpa.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,27 @@ public class ProductService {
 	
 	public Product saveProduct(Product product) {
 		return productRepository.save(product);
+	}
+	
+	public List<Product> getProducts() {
+		return productRepository.findAll();
+	}
+	
+	public Product getById(int id) {
+		return productRepository.findById(id).orElse(null);
+	}
+	
+	public String deleteProduct(int id) {
+		productRepository.deleteById(id);
+		return "product deleted";
+	}
+	
+	public Product updateProduct(Product product) {
+		Product existingProduct= productRepository.findById(product.getId()).orElse(null);
+		existingProduct.setName(product.getName());
+		existingProduct.setQuantity(product.getQuantity());
+		existingProduct.setPrice(product.getPrice());
+		return productRepository.save(existingProduct);
 	}
 	
 }
